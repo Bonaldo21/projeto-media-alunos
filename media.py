7ae5bfa3c550986a9eaa9eda9a0bc8c443952a69
RaizinRaizen <raicampos3246@gmail.com>
2024-11-13 10:39:00 -0300

    Recebendo nota do aluno

---
+++ media.py
@@ -0,0 +1,4 @@
+print("Oi, tudo bem? Vou calcular a media para voce!")
+float(input("Me fale sua primeira média: "))
+float(input("Me fale sua segunda média: "))
+float(input("Me fale sua terceira média: "))
