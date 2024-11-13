93ce714697713ccbf46b2ee87c06c944cf06864f
Bonaldo21 <cauabonaldo@outlook.com>
2024-11-13 11:24:17 -0300

    adicionando recuperação

--- media.py
+++ media.py
@@ -5,7 +5,10 @@ n3 =float(input("Me fale sua terceira média: "))
 media = n1 + n2 +n3 
 mediat = media/3
 print(f"Sua média é {mediat}")
-if mediat >= 6:
+if mediat >= 6.0:
     print("Voce passou! ")
+elif mediat >= 5.0 and mediat < 6.0:
+    print("Voce ficou de recuperação!")
+
 else:
     print("Voce foi reprovado!")
