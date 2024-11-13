d100df306d314c954a4936edd7950bced261fd06
Bonaldo21 <cauabonaldo@outlook.com>
2024-11-13 10:56:23 -0300

    passou ou nao

--- media.py
+++ media.py
@@ -5,3 +5,7 @@ n3 =float(input("Me fale sua terceira média: "))
 media = n1 + n2 +n3 
 mediat = media/3
 print(f"Sua média é {mediat}")
+if mediat >= 6:
+    print("Voce passou! ")
+else:
+    print("Voce foi reprovado!")
