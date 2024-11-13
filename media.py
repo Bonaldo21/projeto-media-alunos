809dc61472171738dc4718a4c83d26d27e2289fc
Bonaldo21 <cauabonaldo@outlook.com>
2024-11-13 11:33:30 -0300

    adicionando media ponderada

--- media.py
+++ media.py
@@ -2,8 +2,8 @@ print("Oi, tudo bem? Vou calcular a media para voce!")
 n1 = float(input("Me fale sua primeira média: "))
 n2 = float(input("Me fale sua segunda média: "))
 n3 =float(input("Me fale sua terceira média: "))
-media = n1 + n2 +n3 
-mediat = media/3
+media = n1 * 1 + n2 * 1 + n3 *2
+mediat = media/4
 print(f"Sua média é {mediat}")
 if mediat >= 6.0:
     print("Voce passou! ")
